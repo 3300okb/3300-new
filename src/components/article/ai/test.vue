b438bde16454c505eb1e48a2cb3689d3cc997e79
Tomokazu Okubo <tomokazuokubo@TomokazunoMacBook-Pro.local>
2026-02-04 03:18:44 +0900

    test

---
+++ src/components/article/ai/test.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export const metadata = {
+  updateDate: '2026/02/04',
+}
+</script>
+
+<script setup lang="ts">
+import ArticleHeader from '@/components/ArticleHeader.vue'
+</script>
+
+<template>
+  <ArticleHeader title="test" :update-date="metadata.updateDate" />
+  test
+</template>
