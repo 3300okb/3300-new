f9e327daabbe0f3a34275353388608f1bd5fafd7
Tomokazu Okubo <tomokazuokubo@TomokazunoMacBook-Pro.local>
2026-02-07 18:05:16 +0900

    add docker

--- src/components/article/etc/docker.vue
+++ src/components/article/etc/docker.vue
@@ -14,9 +14,20 @@ import PreCodes from '@/components/PreCodes.vue'
 
   <PreCodes>
     <pre><b class="t01">docker --version</b>
-<b class="t01">docker run</b> <b class="t03">{XXX}</b> <b class="t02">// Make a container</b></pre>
 
-    <pre><b class="t01">docker images</b> <b class="t02">// Check images</b>
-<b class="t01">docker ps -a</b> <b class="t02">// Check containers</b></pre>
+<b class="t01">docker run</b> <b class="t03">{XXX}</b> <b class="t02">// Make a container</b>
+<b class="t01">docker run</b> <b class="t03">-d --name &#123;XXX&#125;</b> <b class="t01">alpine</b> <b class="t05">sh -c</b> <b class="t04">'while true; do date; sleep 5; done'</b> <b class="t02">// Make a continuously moving container</b>
+<b class="t02">// -d : detached mode</b>
+
+<b class="t01">docker images</b> <b class="t02">// Check images</b>
+
+<b class="t01">docker ps -a</b> <b class="t02">// Check all (stopped, running) containers</b>
+<b class="t01">docker ps</b> <b class="t02">// Check only running containers</b>
+
+<b class="t01">docker logs</b> <b class="t03">--tail 5 &#123;XXX&#125;</b> <b class="t02">// Check the last five lines of the log</b>
+
+<b class="t01">docker stop</b> <b class="t03">&#123;XXX&#125;</b> <b class="t02">// Stop a container</b>
+
+<b class="t01">docker rm</b> <b class="t03">&#123;XXX&#125;</b> <b class="t02">// Remove a container</b></pre>
   </PreCodes>
 </template>
