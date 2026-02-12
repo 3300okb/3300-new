f7047115c52ab4e956f9d75a6b49bbe2c8960706
Tomokazu Okubo <ohkubo@lycorp.co.jp>
2026-02-12 13:10:47 +0900

    edit custom scrollbar

--- src/components/article/css/custom-scrollbar.vue
+++ src/components/article/css/custom-scrollbar.vue
@@ -14,16 +14,21 @@ import CopyCode from '@/components/CopyCode.vue'
   <ArticleHeader title="custom-scrollbar" :update-date="metadata.updateDate" />
 
   <PreCodes>
-    <pre><code><CopyCode><b class="t01">&amp;::-webkit-scrollbar &#123;
-  height: rem(6);
+    <pre><code><CopyCode><b class="t01"><b class="t02">// Firefox</b>
+scrollbar-width: <b class="t03">thin</b>;
+scrollbar-color: <b class="t03">#bcc4d2 #fff</b>;
+
+<b class="t02">// Chrome・Safari・Edge</b>
+&amp;::-webkit-scrollbar &#123;
+  height: <b class="t03">0.375rem</b>;
 &#125;
-&amp;::-webkit-scrollbar<b class="t03">-track</b> &#123;
-  border-radius: 9999;
-  background-color: #fff;
+&amp;::-webkit-scrollbar<b class="t04">-track</b> &#123;
+  border-radius: <b class="t03">9999</b>;
+  background-color: <b class="t03">#fff</b>;
 &#125;
-&amp;::-webkit-scrollbar<b class="t03">-thumb</b> &#123;
-  border-radius: 9999;
-  background-color: #bcc4d2;
+&amp;::-webkit-scrollbar<b class="t05">-thumb</b> &#123;
+  border-radius: <b class="t03">9999</b>;
+  background-color: <b class="t03">#bcc4d2</b>;
 &#125;</b></CopyCode>
 </code></pre>
   </PreCodes>
